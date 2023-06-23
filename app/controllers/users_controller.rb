6903eee29cede5d0f7dd59351c25c77184ae3523
viktor <viktor.saprykin@warwick.ac.uk>
2023-06-23 09:08:05 +0100

    Ch_10,v1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless
+      current_user?(@user)
   end
 
   def show
@@ -11,6 +24,10 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,7 +36,27 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-    render 'new', status: :unprocessable_entity
+      render 'new', status: :unprocessable_entity
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
     end
-end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+    end
+    redirect_to login_url, status: :see_other
+
+
+
+  end end
